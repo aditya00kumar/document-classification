50a3f42c434be59981ddfe574db138026ab020d0
Aditya kumar <aditya00kumar@users.noreply.github.com>
2019-03-11 14:09:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+Author: Aditya Kumar
+Email: aditya00kumar@gmail.com
+Description: All configuration parameters for Flask app
+Project: Document_Classification
+Last Modified: 1/9/18 6:44 PM
+"""
+class Config(object):
+    DEBUG = True
+    # host = 'localhost'
+    # host = '10.216.7.223'
+    UPLOAD_FOLDER = '/Users/aditya1/Documents/Document_Classification/Uploads'
+    MAX_CONTENT_LENGTH = 16*1024*1024
+    # DEVELOPMENT = True
+    # SECRET_KEY = 'do-i-really-need-this'
+    # FLASK_HTPASSWD_PATH = ''
+    # FLASK_SECRET = SECRET_KEY
+    # DB_HOST = 'database' # a docker link
